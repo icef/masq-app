35a2f5ca93810510433ebb922261b3b3b2ceacae
Arne Eilermann <eilermann@lavabit.com>
2012-09-13 16:52:23 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasqApp::Application.config.secret_token = 'c1473932a6ac22900edf9d90f26ecbd9eae72c825ca27f5381926d9682f9ea4bbd4e4beb5ae6e70795788a1e0d909b89c2725932f6faf7abcc10b716a8378311'

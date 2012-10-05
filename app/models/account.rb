214e488a29e2cd8c74fb35cdb9c6ee6d9b7361f0
Arne Eilermann <eilermann@lavabit.com>
2012-10-05 15:38:32 +0200

    Moved account validations to masq-app.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,24 @@
 class Account < ActiveRecord::Base
+  has_one :masq_account, :class_name => 'Masq::Account'
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable , :trackable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of   :login
+  validates_length_of     :login, :within => 3..254
+  validates_uniqueness_of :login, :case_sensitive => false
+  validates_format_of     :login, :with => /^[A-Za-z0-9_@.-]+$/
+  validates_presence_of   :email
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates_format_of     :email, :with => Devise.email_regexp
+
+  validates_presence_of     :password                                    #, :if => :password_required?
+  validates_presence_of     :password_confirmation                       #, :if => :password_required?
+  validates_length_of       :password, :within => Devise.password_length #, :if => :password_required?
+  validates_confirmation_of :password                                    #, :if => :password_required?
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :login, :password, :password_confirmation, :remember_me
